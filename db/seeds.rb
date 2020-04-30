d55ac07a901cc9a99520680fd544f71e11cd7a83
lgentaz <lucile.gentaz@gmail.com>
2020-04-30 23:57:26 +0200

    faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+10.times do
+    user = User.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        description: Faker::Hipster.sentence,        
+        age: rand(18..124)
+    )
+    user.email = Faker::Internet.free_email(name: "%#{user.first_name}%"),
+end
+
